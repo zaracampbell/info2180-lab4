0544cdb0fb691e0ed33e5981a6cd6442f347ebcd
zaracampbell <campbellzara13@gmail.com>
2024-11-19 15:31:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+document.getElementById("search").addEventListener("click", function () {
+    // Make an AJAX call
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "superheroes.php", true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            alert(xhr.responseText); // Show the superheroes in an alert
+        } else {
+            alert("An error occurred while fetching superheroes.");
+        }
+    };
+    xhr.send();
+});
 
